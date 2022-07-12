ea7b8dff5dd138635008d48b75276edfc3853099
L-zhicong <779139241@qq.com>
2022-07-12 11:30:25 +0800

    文章数据

--- src/views/article/data.js
+++ src/views/article/data.js
@@ -1,3 +1,96 @@
+// 文章列表
+export const articleTitle = [
+  {
+    label: '文章ID',
+    type: 'commonData',
+    prop: 'id',
+    width: '70'
+  },
+  {
+    label: '文章名称',
+    type: 'commonData',
+    prop: 'name',
+    width: '120'
+  },
+  {
+    label: '文章LOGO',
+    type: 'coverData',
+    prop: 'logo',
+    width: '130'
+  },
+  {
+    label: '文章标题',
+    type: 'commonData',
+    prop: 'title',
+    width: '110'
+  },
+  {
+    label: '摘要',
+    type: 'commonData',
+    prop: 'abstract',
+    width: '120'
+  },
+  {
+    label: '关联词',
+    type: 'commonData',
+    prop: 'related_words',
+    width: '120'
+  },
+  {
+    label: '来源',
+    type: 'commonData',
+    prop: 'source',
+    width: '95'
+  },
+  {
+    label: '作者',
+    type: 'commonData',
+    prop: 'author',
+    width: '95'
+  },
+  {
+    label: '阅读人数',
+    type: 'numData',
+    prop: 'read_num',
+    width: '70'
+  },
+  {
+    label: '点赞人数',
+    type: 'numData',
+    prop: 'FabulousNum',
+    width: '70'
+  },
+  {
+    label: '状态',
+    type: 'tagData',
+    prop: 'status',
+    formatColor: { 1: '', 2: 'info' },
+    formatText: { 1: '发布', 2: '未发布' },
+    width: '95'
+  },
+  {
+    label: '创建时间',
+    type: 'commonData',
+    prop: 'create_time',
+    width: '160'
+  },
+  {
+    label: '发布时间',
+    type: 'timeData',
+    prop: 'release_time',
+    width: '190'
+  },
+  {
+    label: '操作',
+    type: 'operation',
+    operation: [
+      { btnText: '查看文章', btnType: 'primary', clickFun: 'seeArticle', condition: () => null },
+      { btnText: '编辑', btnType: 'warning', clickFun: 'updateArticle', condition: () => null },
+      { btnText: '删除', btnType: 'info', clickFun: 'delArticle', condition: () => null }
+    ],
+    width: '220'
+  }
+]
 // 文章分类
 export const categoryTitle = [
   {
