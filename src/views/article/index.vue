cf9daf13f4c897dbe9241fc8ab1febdedd5b10b3
L-zhicong <779139241@qq.com>
2022-07-14 22:04:57 +0800

    文章

---
+++ src/views/article/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app-container">
+    <Title @fetchData="fetchData" />
+    <Table ref="Table" />
+  </div>
+</template>
+
+<script>
+import Title from './comps/Title'
+import Table from './comps/Table'
+export default {
+  name: 'Article',
+  components: {
+    Title,
+    Table
+  },
+  methods: {
+    fetchData() {
+      this.$refs.Table.fetchData()
+    }
+  }
+}
+</script>
