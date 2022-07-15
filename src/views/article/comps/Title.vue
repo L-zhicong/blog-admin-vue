c9d943f52d88e0824740ae3808de1d7eac3d92cc
L-zhicong <779139241@qq.com>
2022-07-15 22:45:38 +0800

    文章

---
+++ src/views/article/comps/Title.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="search-title">
+      <div class="search-title-left">
+        <el-button size="mini" type="primary" @click="addArticle">添加文章</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    addArticle() {
+      this.$nextTick(() => {
+        this.$router.push({ path: `/article/list/add` })
+      })
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.search-title {
+  margin-bottom: 20px;
+}
+</style>
