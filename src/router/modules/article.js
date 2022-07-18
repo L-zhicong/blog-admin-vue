2c52a42b0cf6809f4ec6b81a9ffc7fcb014c7015
L-zhicong <779139241@qq.com>
2022-07-18 17:33:32 +0800

    文章路由

--- src/router/modules/article.js
+++ src/router/modules/article.js
@@ -7,6 +7,26 @@ const articleRouter = {
   meta: { title: '文章管理' },
   children: [
     {
+      path: '/article/list',
+      name: 'articleList',
+      component: () => import('@/views/article/index'),
+      meta: { title: '文章列表' }
+    },
+    {
+      path: '/article/list/edit/:id(\\d+)',
+      name: 'rticleEdit',
+      component: () => import('@/views/article/details/index'),
+      meta: { title: '编辑文章', activeMenu: '/article/list' },
+      hidden: true
+    },
+    {
+      path: '/article/list/add',
+      name: 'rticleAdd',
+      component: () => import('@/views/article/details/index'),
+      meta: { title: '添加文章', activeMenu: '/article/list' },
+      hidden: true
+    },
+    {
       path: '/article/category',
       name: 'articleCategory',
       component: () => import('@/views/article/category/index.vue'),
