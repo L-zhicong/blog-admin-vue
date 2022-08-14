be6a5d7d65ecd705db52548d355f01210d0bd35a
L-zhicong <779139241@qq.com>
2022-08-14 22:34:09 +0800

    管理主页

---
+++ src/views/myhomepage/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="intro" style="">
+    <div class="container">
+      <div class="row">
+        <div class="intro-text">
+          <audio id="bgmusic" src="/music/蓝心羽 - 还是会想你 (Live).mp3" controls="controls" autoplay="autoplay" />
+          <h1>你好，我是<span class="name">{$about_me.me_name}</span></h1>
+          <p>{$about_me.me_remarks}</p>
+          <a href="#about" class="btn btn-default btn-lg page-scroll">了解更多</a> </div>
+      </div>
+    </div>
+  </div>
+</template>
